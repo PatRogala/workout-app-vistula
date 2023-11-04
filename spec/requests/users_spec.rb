52c06a76f36cda38d0d0dd04eead659affe36ff7
Patryk Rogala <rogala.patryk@pm.me>
2023-11-04 10:11:48 +0100

    Add User Controller

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+  describe "GET /index" do
+    context "when user is not logged in" do
+      it "redirects to login page" do
+        get root_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "when user is logged in" do
+      before { sign_in create(:user) }
+
+      it "returns http success" do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
+end
